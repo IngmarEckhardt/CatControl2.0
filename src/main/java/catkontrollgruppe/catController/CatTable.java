8d6ac11d0cba27efb64e8b13e25ead36acb4510b
IngmarEckhardt <88197507+IngmarEckhardt@users.noreply.github.com>
2021-08-03 01:43:38 +0200

    Add files via upload

--- src/main/java/catkontrollgruppe/catController/CatTable.java
+++ src/main/java/catkontrollgruppe/catController/CatTable.java
@@ -1,17 +1,8 @@
 package catkontrollgruppe.catController;
 
-import catkontrollgruppe.catcontrolService.Cat;
-import catkontrollgruppe.catcontrolService.CatService;
-import catkontrollgruppe.catcontrolService.CatcontainerMonitor;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableArray;
-import javafx.collections.ObservableList;
-import java.util.ArrayList;
-
 import static javafx.application.Application.launch;
 
 public class CatTable {
-    private int catcount;
     private boolean safeFlag;
 
     //constructor
@@ -19,13 +10,6 @@ public class CatTable {
         launch();
     }
 
-    public int getCatcount() {
-        return catcount;
-    }
-
-    public void setCatcount(int catcount) {
-        this.catcount = catcount;
-    }
 
     public void start() {
     }
