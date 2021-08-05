08c4ad6887ce52fa2102791b850871101b746ab6
IngmarEckhardt <88197507+IngmarEckhardt@users.noreply.github.com>
2021-08-05 08:25:45 +0200

    Add files via upload

--- src/main/java/catkontrollgruppe/catcontrolService/CatSpeichermanager.java
+++ src/main/java/catkontrollgruppe/catcontrolService/CatSpeichermanager.java
@@ -12,24 +12,24 @@ public class CatSpeichermanager {
     public CatSpeichermanager() {
     }
 
-    public void writeCats(final ObservableList<Cat> catArray) {
+    public void writeCats(ObservableList<Cat> catArray) {
         try {
-            final File datei = new File(System.getProperty("user.home") + File.separator + "Cats.json");
-            final ObjectMapper mapper = new ObjectMapper();
+            File datei = new File(System.getProperty("user.home") + File.separator + "Cats.json");
+            ObjectMapper mapper = new ObjectMapper();
             mapper.writeValue(datei, catArray);
-        } catch (final IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     protected ArrayList<Cat> readCats() {
-        final ArrayList<Cat> catlist = new ArrayList();
-        final ObjectMapper mapper = new ObjectMapper();
-        final File datei = new File(System.getProperty("user.home") + File.separator + "Cats.json");
+        ArrayList<Cat> catlist = new ArrayList();
+        ObjectMapper mapper = new ObjectMapper();
+        File datei = new File(System.getProperty("user.home") + File.separator + "Cats.json");
         try {
             catlist.addAll(Arrays.asList(mapper.readValue(datei, Cat[].class)));
 
-        } catch (final IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return catlist;
--- src/main/java/catkontrollgruppe/catcontrolService/Catcontainer.java
+++ src/main/java/catkontrollgruppe/catcontrolService/Catcontainer.java
@@ -3,23 +3,24 @@ package catkontrollgruppe.catcontrolService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-import javax.swing.*;
-
 public class Catcontainer {
     private ObservableList<Cat> catlist;
     protected CatCache catCache = new CatCache();
     protected static boolean intendedDelete;
 
-    public Catcontainer() {}
+    public Catcontainer() {
+    }
 
+    public static void main(String[] args) {
+        CatCache cache = new CatCache();
+        cache.start();
+    }
 
     public void CatKreation(final String name, final int alter, final String impfdatum, final double gewicht, final boolean rund, final boolean suess) {
 
-        final Cat newCat = new Cat(name, alter, impfdatum, gewicht, rund, suess);
+        Cat newCat = new Cat(name, alter, impfdatum, gewicht, rund, suess);
         this.addCat(newCat);
         this.catlist = FXCollections.observableArrayList(this.catCache.getCatArray());
-        JOptionPane.showMessageDialog(null, "Die Katze " + newCat + " wurde erzeugt und dem Datensatz " +
-                "hinzugefügt", "Eingabe erfolgreich", JOptionPane.INFORMATION_MESSAGE);
     }
 
     protected void addCat(final Cat newCat) {
